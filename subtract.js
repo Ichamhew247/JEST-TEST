7676dfbcf145a3bd2645151885416eef325d706d
Ichamhew247 <nattanicha027@gmail.com>
2023-07-06 22:45:42 +0700

    set up and try test subtrack

---
+++ subtract.js
@@ -0,0 +1,11 @@
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = subtract;
+
+// const subtract = require("./subtract");
+
+// test("Must subtract properly", () => {
+//   expect(subtract(1, 2)).toBe(-1);
+// });
